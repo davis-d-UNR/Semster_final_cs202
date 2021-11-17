7e7530cd3f15c4003622d061dff13fc6610de823
Brie Wiles <bwiles@Bries-MacBook-Air.local>
2021-11-17 11:10:59 -0800

    nothing

--- wav.cpp
+++ wav.cpp
@@ -15,3 +15,20 @@ void Wav::writeFile(const std::string &filename){
 
     
 }
+
+
+waveHeader.sample_rate[] = toStruct[];
+waveHeader.byte_rate[] = toStruct[];
+waveHeader.sample_alignment[] = toStruct[];
+waveHeader.bit_depth[] = toStruct[];
+
+
+waveHeader.data_header[0] = toStruct[];
+waveHeader.data_header[1] = toStruct[];
+waveHeader.data_header[2] = toStruct[];
+waveHeader.data_header[3] = toStruct[];
+
+
+waveHeader.sample_rate[] = toStruct[];
+waveHeader.data_bytes[] = toStruct[];
+
