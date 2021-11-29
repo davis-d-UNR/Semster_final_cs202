e2998de4486b943b08a264309be332cb019f5f17
Brie Wiles <bwiles@Bries-MacBook-Air.local>
2021-11-29 13:03:07 -0800

    normalization started

--- normalization.cpp
+++ normalization.cpp
@@ -2,7 +2,12 @@
 
 void Normalization::processeBuffer(unsigned char* buffer, size_t bufferSize, int scale) 
 {
-     std::cout << "normalization effect" << std::endl;
+     unsigned char* outputBuffer;
 
+     for(int i = 0; i < bufferSize; i++){
+     auto maxValue = std::max_element(buffer)
+
+
+     }
 }
 
