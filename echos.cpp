ba8937e7aee8e0ba55abf3b6d7b2672a4d3c2df6
Brie Wiles <bwiles@Bries-MacBook-Air.local>
2021-11-29 12:21:48 -0800

    Echo for loop

--- echos.cpp
+++ echos.cpp
@@ -2,6 +2,14 @@
 
 void Echo::processeBuffer(unsigned char* buffer, size_t bufferSize, int scale)
 {
-  
-
+  int gain = 2;
+  int delay = 1;
+  unsigned char* outputBuffer;
+  for(int i = 0; i < bufferSize; i++){
+      if(i < delay){
+        outputBuffer[i] = (buffer[i] + gain * outputBuffer[i - delay]);
+      } else {
+        outputBuffer[i] = buffer[i];
+      }
+  };
 }
