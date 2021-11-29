370a0e1385f6128b4299bce8e38e01388415312d
Brie Wiles <bwiles@Bries-MacBook-Air.local>
2021-11-29 14:02:52 -0800

    normalization

--- normalization.cpp
+++ normalization.cpp
@@ -1,11 +1,17 @@
 #include "normalization.h"
+#include <fstream>
+#include <utility>
+#include <algorithm>
+
+
 
 void Normalization::processeBuffer(unsigned char* buffer, size_t bufferSize, int scale) 
 {
      unsigned char* outputBuffer;
+     float value;
 
      for(int i = 0; i < bufferSize; i++){
-     auto maxValue = std::max_element(buffer)
+          value = scale * (1 / *max_element(buffer, buffer + bufferSize));
 
 
      }
